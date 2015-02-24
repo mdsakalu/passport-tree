38a55443ba469e44ed94dd6a7287afdbe15dfd30
slackey <slackey@gmail.com>
2015-02-24 21:00:27 +0000

    turn off silent

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var forever = require('forever-monitor');
 
-  var child = new (forever.Monitor)('index.js', {
-    silent: true,
-  });
+  var child = new (forever.Monitor)('index.js');
 
   child.start();
